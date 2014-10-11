228efbd44119d74a08b68a6325db2c01239ddf02
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2014-10-10 21:41:21 -0300

    Agregue excepciones y validaciones en el FrmLogIn (dejo de funcionar :( , hay algo que es null en algun lado, me duele la cabeza como para seguir, necesito descanzar unas horas :P )

--- src/FrbaHotel/Vistas/Login/FormElegirRol.cs
+++ src/FrbaHotel/Vistas/Login/FormElegirRol.cs
@@ -11,9 +11,12 @@ namespace FrbaHotel.Vistas.Login
 {
     public partial class FormElegirRol : Form
     {
+        int idUsuarioQueMandoElLogIn;
+    
         public FormElegirRol()
         {
             InitializeComponent();
+            this.idUsuarioQueMandoElLogIn = Program.idUsuario;//en teoria esta variable de clase ya esta asignada cuando se llega a esta linea
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
--- src/FrbaHotel/Vistas/Login/FrmLogIn.cs
+++ src/FrbaHotel/Vistas/Login/FrmLogIn.cs
@@ -15,6 +15,8 @@ namespace FrbaHotel.Login
     public partial class FrmLogIn : Form
     {
         SqlConnection conexion = BaseDeDatos.ObtenerConexion();
+        int contador = 3;
+        string usuarioVerificar = null;
     
         public FrmLogIn()
         {
@@ -50,12 +52,101 @@ namespace FrbaHotel.Login
         private void entrarBtn_Click(object sender, EventArgs e)
         {
             String consultaUsuario = "SELECT id_usuario, username, password, intentos_fallidos" +
-               "FROM AEFI.tl_usuario " +
-               "WHERE username = @username";
+                                     "FROM AEFI.TL_Usuario " +
+                                     "WHERE username = @username";
 
-            Form formElegirRol = new FormElegirRol();//por ahora esta asi, pero aca se le tiene que mandar el id_usuario
-            formElegirRol.Show();
-            this.Hide();
+            try
+            {
+                conexion.Open(); //me parece que esto esta de mas aca o esta bien aca y la BaseDeDatos lo hace de mas :P
+                SqlCommand comando = new SqlCommand(consultaUsuario, conexion);
+                comando.Parameters.Add(new SqlParameter("@usuario", txbUsuario.Text));
+                SqlDataReader reader = comando.ExecuteReader();
+
+                // chequeo que exista el usuario ingresado
+                if (!reader.HasRows)
+                {
+                    throw new Excepciones("Usuario inexistente");
+                }
+
+                else
+                {
+                    reader.Read();
+                    int idUsuario = Convert.ToInt32(reader["id_usuario"]);
+                    bool primerIngreso = (bool)reader["pass_temporal"];
+                    bool habilitado = (bool)reader["habilitado"];
+                    string usuario = reader["username"].ToString();
+                    string contrasena = reader["password"].ToString();
+                    reader.Close();
+
+                    if (!contrasena.Equals(Utilidades.cifrar256(txbContrasena.Text)))
+                    {
+                        if (usuarioVerificar == null || usuarioVerificar == usuario)
+                        {
+                            contador--;
+                        }
+                        else
+                        {
+                            contador = 2;
+                        }
+
+                        usuarioVerificar = usuario;
+                        throw new Excepciones("Contraseña incorrecta");
+                    }
+
+                    if (!habilitado)
+                    {
+                        throw new Excepciones("Usuario inhabilitado");
+                    }
+
+                    if (primerIngreso)
+                    {
+                        //CambioContrasena ingreso = new CambioContrasena(usuario);
+                        //ingreso.ShowDialog();
+                    }
+
+                    txbContrasena.Enabled = false;
+
+                    // se cargan las variables globales del sistema
+                    Program.idUsuario = idUsuario;
+                    Program.usuario = usuario;
+                }
+
+                MessageBox.Show("Eliga un Rol", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form formElegirRol = new FormElegirRol();
+                this.Hide();
+                formElegirRol.ShowDialog(); //o Show()?
+                this.Close();
+                
+            }
+            catch (Excepciones exc)
+            {
+                MessageBox.Show(exc.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // si ocurrieron 3 intentos fallidos en ingresar la contraseña se inhabilita al usuario
+                if (contador == 0)
+                {
+                    string consultaInhabilitar = "UPDATE AEFI.TL_Usuario" +
+                        "SET habilitado = 0 " +
+                        "WHERE username = @username";
+                    SqlCommand comandoInhabilitar = new SqlCommand(consultaInhabilitar, conexion);
+                    comandoInhabilitar.Parameters.Add(new SqlParameter("@usuario", txbUsuario.Text));
+                    comandoInhabilitar.ExecuteNonQuery();
+                    MessageBox.Show("El usuario ha sido inhabilitado por ingresos invalidos", "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (SqlException exc)
+            {
+                MessageBox.Show(exc.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+            {
+                conexion.Close();
+            }
+            
+           
         }
 
         private void txbUsuario_TextChanged(object sender, EventArgs e)
