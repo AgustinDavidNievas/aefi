0d10b9e51ae5b3ef94906b1cfcfb72d27c55a9f0
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2014-10-25 11:32:49 -0300

    no se habia commiteado todo xD

--- src/FrbaHotel/Vistas/ABM de Cliente/FormClienteNuevo.Designer.cs
+++ src/FrbaHotel/Vistas/ABM de Cliente/FormClienteNuevo.Designer.cs
@@ -279,6 +279,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
