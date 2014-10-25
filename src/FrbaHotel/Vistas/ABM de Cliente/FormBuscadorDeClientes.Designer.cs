f85553172a382b4827e283c5930443df188cef97
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2014-10-25 11:26:10 -0300

    Comportamiento en el FormBuscadorDeClientes, faltan implementar 2 botones porque todavia no se puden probar al faltar las tablas :P

--- src/FrbaHotel/Vistas/ABM de Cliente/FormBuscadorDeClientes.Designer.cs
+++ src/FrbaHotel/Vistas/ABM de Cliente/FormBuscadorDeClientes.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbApellido = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txbDocumento = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbTipoDeDocumento = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txbMail = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buscarButton = new System.Windows.Forms.Button();
+            this.modificarButton = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -66,19 +66,19 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Apellido";
             // 
-            // textBox1
+            // txbNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 20);
-            this.textBox1.TabIndex = 2;
+            this.txbNombre.Location = new System.Drawing.Point(63, 12);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(183, 20);
+            this.txbNombre.TabIndex = 2;
             // 
-            // textBox2
+            // txbApellido
             // 
-            this.textBox2.Location = new System.Drawing.Point(302, 13);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(183, 20);
-            this.textBox2.TabIndex = 3;
+            this.txbApellido.Location = new System.Drawing.Point(302, 13);
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(183, 20);
+            this.txbApellido.TabIndex = 3;
             // 
             // label3
             // 
@@ -89,12 +89,13 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Documento";
             // 
-            // textBox3
+            // txbDocumento
             // 
-            this.textBox3.Location = new System.Drawing.Point(81, 50);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(165, 20);
-            this.textBox3.TabIndex = 5;
+            this.txbDocumento.Location = new System.Drawing.Point(81, 50);
+            this.txbDocumento.Name = "txbDocumento";
+            this.txbDocumento.Size = new System.Drawing.Size(165, 20);
+            this.txbDocumento.TabIndex = 5;
+            this.txbDocumento.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label4
             // 
@@ -105,13 +106,13 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Tipo de documento";
             // 
-            // comboBox1
+            // cbTipoDeDocumento
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(357, 49);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(128, 21);
-            this.comboBox1.TabIndex = 7;
+            this.cbTipoDeDocumento.FormattingEnabled = true;
+            this.cbTipoDeDocumento.Location = new System.Drawing.Point(357, 49);
+            this.cbTipoDeDocumento.Name = "cbTipoDeDocumento";
+            this.cbTipoDeDocumento.Size = new System.Drawing.Size(128, 21);
+            this.cbTipoDeDocumento.TabIndex = 7;
             // 
             // label5
             // 
@@ -122,12 +123,12 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Mail";
             // 
-            // textBox4
+            // txbMail
             // 
-            this.textBox4.Location = new System.Drawing.Point(63, 83);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(183, 20);
-            this.textBox4.TabIndex = 9;
+            this.txbMail.Location = new System.Drawing.Point(63, 83);
+            this.txbMail.Name = "txbMail";
+            this.txbMail.Size = new System.Drawing.Size(183, 20);
+            this.txbMail.TabIndex = 9;
             // 
             // dataGridView1
             // 
@@ -146,6 +147,7 @@
             this.button1.TabIndex = 11;
             this.button1.Text = "Eliminar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -156,23 +158,25 @@
             this.button2.Text = "Limpiar";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buscarButton
             // 
-            this.button3.Location = new System.Drawing.Point(409, 278);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Buscar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buscarButton.Location = new System.Drawing.Point(409, 278);
+            this.buscarButton.Name = "buscarButton";
+            this.buscarButton.Size = new System.Drawing.Size(75, 23);
+            this.buscarButton.TabIndex = 13;
+            this.buscarButton.Text = "Buscar";
+            this.buscarButton.UseVisualStyleBackColor = true;
+            this.buscarButton.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // modificarButton
             // 
-            this.button4.Location = new System.Drawing.Point(166, 278);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "Modificar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.modificarButton.Location = new System.Drawing.Point(166, 278);
+            this.modificarButton.Name = "modificarButton";
+            this.modificarButton.Size = new System.Drawing.Size(75, 23);
+            this.modificarButton.TabIndex = 14;
+            this.modificarButton.Text = "Modificar";
+            this.modificarButton.UseVisualStyleBackColor = true;
+            this.modificarButton.Click += new System.EventHandler(this.modificarButton_Click);
             // 
             // button5
             // 
@@ -182,6 +186,7 @@
             this.button5.TabIndex = 15;
             this.button5.Text = "Habilitar";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -191,6 +196,7 @@
             this.button6.TabIndex = 16;
             this.button6.Text = "Volver";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // FormBuscadorDeClientes
             // 
@@ -199,23 +205,24 @@
             this.ClientSize = new System.Drawing.Size(508, 324);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.modificarButton);
+            this.Controls.Add(this.buscarButton);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txbMail);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbTipoDeDocumento);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txbDocumento);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.txbNombre);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FormBuscadorDeClientes";
             this.Text = "FormBuscadorDeClientes";
+            this.Load += new System.EventHandler(this.FormBuscadorDeClientes_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -226,19 +233,19 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbApellido;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txbDocumento;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbTipoDeDocumento;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txbMail;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buscarButton;
+        private System.Windows.Forms.Button modificarButton;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
     }
